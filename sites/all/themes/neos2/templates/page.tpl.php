140d43fd99be632778eeb27cb05f5f272aa5537c
tolotravalery <valeryharisoanitolotra@gmail.com>
2017-03-20 12:04:31 +0100

    admin bar commit first

--- sites/all/themes/neos2/templates/page.tpl.php
+++ sites/all/themes/neos2/templates/page.tpl.php
@@ -5,6 +5,7 @@
 		</div>
 	</header>
 	<?php print render($page['contenu']); ?>
+	<?php print render($page['tsilana']); ?>
 	<footer>
         <?php print render($page['footer']); ?>
      </footer>
